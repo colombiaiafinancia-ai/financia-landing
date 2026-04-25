fc0bd46889706ab00cae4861fa1ef3025807ad6c
Felipe <colombiaia.financia@gmail.com>
2026-04-25 00:12:26 -0500

    subscription mp

--- app/subscribe/page.tsx
+++ app/subscribe/page.tsx
@@ -2,9 +2,9 @@ import SubscriptionCheckout from "@/components/subscriptions/SubscriptionCheckou
 
 export default function SubscribePage() {
   return (
-    <main className="mx-auto max-w-xl p-6">
+    <main className="3357848177">
       <SubscriptionCheckout
-        userId="88e32420-959a-4415-bdb6-da6ea25a0852"
+        userId="8"
         payerEmail="test-vyk4bfj0v@srv1.mail-tester.com"
         planKey="financia_pro_monthly"
       />
